28fc6eedc67007bd463d7d4f68d4b7782f349403
Andrii Bikliann <biklyan13@gmail.com>
2021-06-12 22:17:14 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <example-login/>
+  </div>
+</template>
+
+<script>
+import ExampleLogin from './components/ExampleLogin.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ExampleLogin
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ExampleLogin.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+      <label>
+      Введіть логін
+      <input type="text" v-model="userLogin">
+      </label> <br>
+      <label>
+      Введіть пароль
+      <input type="password" v-model="userPass">
+      </label> <br>
+      <button @click="checkPass">Go</button>
+      <img :src="imageSrc" alt="picture" v-if="checkPass">
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'ExampleLogin',
+        props: {
+            imageSrc: {
+                type: String,
+                default:"https://image.freepik.com/free-vector/smile-logo-template_8163-40.jpg"
+            }
+        },
+        data () {
+            return {
+               userLogin:"",
+               userPass :"",
+               authResultMessage: null,
+               loginBase : [
+                  {
+                  login: "Ivan", 
+                  pass:"111"
+                  },
+                  {
+                  login: "Petro", 
+                  pass:"222"
+                  },
+                  {
+                  login: "Roma", 
+                  pass:"333"
+                  },
+                  {
+                  login: "Stas", 
+                  pass:"444"
+                  }, 
+                 ],
+            };
+        },
+        methods: {
+        checkPass() {
+      if (
+        this.loginBase.some(
+          (item) => this.userLogin == item.login && this.userPass == item.pass
+          )
+        )
+          this.authResultMessage = "Вірно"
+          else this.authResultMessage = "Некоректний логін або пароль!"
+      }
+    }
+ }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
